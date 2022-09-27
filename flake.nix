281c0b8bf8dd03c0e445d8f5884b36aca154dd95
timothy <git@timothyklim.com>
2022-09-27 12:08:34 +0700

    add nix

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "rust-openssl flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-22.05";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, fenix }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ fenix.overlay ] ++ [
+          (self: super: rec {
+            rust_pinned = self.fenix.default;
+          })
+        ];
+      };
+      rustPlatform = with pkgs; makeRustPlatform { inherit (rust_pinned) cargo rustc; };
+    in
+    rec {
+      devShells.${system}.default = with pkgs; mkShell {
+        buildInputs = [ rust_pinned.toolchain ];
+      };
+      formatter.${system} = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
+    };
+}
